6c0bab71b6215a68c688df06a7046bc26def5e70
edmundonajera4 <edmundonajera4@gmail.com>
2026-08-04 22:43:18 -0600

    fix: eliminar doble BrowserRouter y corregir estructura de router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { AuthProvider } from './AuthProvider'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
+    <BrowserRouter>
       <App />
-    </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
